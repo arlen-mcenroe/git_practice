ad51694ed44af40924d6a5c47d78dc7c25e66b05
arlenmc-kobold <arlen.mcenroe@koboldmetals.com>
2024-06-14 15:58:40 -0700

    Added more dialogue and option for user input

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("Hello World, my name is Roger")
+print("How are you doing today?")
+answer = input("Enter a number: 1 for Good, 2 for Okay, 3 for Bad: ")
+
+if answer == "1":
+    print("I'm glad you're having a good day!")
+elif answer == "2":
+    print("Just ok?")
+elif answer == "13":
+    print("Aw, I'm sorry you're having a bad day!")
+else:
+    print("Answer does not compute.")
